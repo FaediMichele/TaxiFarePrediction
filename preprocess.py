7872eb132e428cba1ad82f694f78b0110c21b418
Francesco <francesco.mistri98@gmail.com>
2023-02-07 16:25:24 +0100

    Complete preprocess script
    Uses the preprocessing pipeline found in util to dump a compressed and cleaned up version of the dataset.

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 """Preprocess the given dataset and dump it to a parquet file."""
 import argparse
+import util
 
 DESCRIPTION = __doc__
 
@@ -11,11 +12,15 @@ class Namespace:
 
 
 def main(namespace: Namespace):
-    pass
+    """Process the given arguments."""
+    df = util.load_data(namespace.input_path)
+    df.sink_parquet(namespace.output_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('input_path', type=str, metavar='INPUT_FILE')
+    parser.add_argument('output_path', type=str, metavar='OUTPUT_FILE')
 
     args = parser.parse_args(namespace=Namespace())
     main(args)
