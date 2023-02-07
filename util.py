a19e980b490a006d34cc574b7f304bd35f4a7981
Francesco <francesco.mistri98@gmail.com>
2023-02-07 16:13:08 +0100

    Add explicit path parameter to load_data()
    Still defaults to the constant DATASET_PATH.

--- util.py
+++ util.py
@@ -7,8 +7,8 @@ NEW_YORK_AREA = [(40.506797, 41.130785), (-74.268086, -73.031593)]
 
 
 
-def load_data() -> pl.LazyFrame:
-    return pl.scan_csv(DATASET_PATH).filter(
+def load_data(dataset_path=DATASET_PATH) -> pl.LazyFrame:
+    return pl.scan_csv(dataset_path).filter(
         (pl.col("pickup_longitude").is_between(*NEW_YORK_AREA[1], closed='both')) &
         (pl.col("pickup_latitude").is_between(*NEW_YORK_AREA[0], closed='both')) &
         (pl.col("dropoff_longitude").is_between(*NEW_YORK_AREA[1], closed='both')) &
