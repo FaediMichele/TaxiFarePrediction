7bc4795ea3770f9bfc38f96fa9571e2d0e5410b3
FaediMichele <work.faedi.michele@gmail.com>
2023-02-08 19:08:56 +0100

    Corrected the image. Now it cast the points_area to a square in order to get a 1/1 aspect ratio image. And crop the image to have the points with the correct origin(actually there is a bit of error to north).

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import math
 import polars as pl
 import staticmaps
 import matplotlib.pyplot as plt
+import math
 
 DATASET_PATH = 'datasets/train.csv'
 NEW_YORK_AREA = [(40.506797, 41.130785), (-74.268086, -73.031593)]
@@ -29,18 +30,49 @@ def get_image_from_coordinate(points_area, sizes):
         (down, left),
         (down, right),
         (up, right),
-        (up, left)
+        (up, left),
+        (down, left)
     ]
 
     context.add_object(
         staticmaps.Area(
             [staticmaps.create_latlng(lat, lng) for lat, lng in polygon],
             fill_color=staticmaps.TRANSPARENT,
-            width=0,
-            color=staticmaps.TRANSPARENT,
+            width=2,
+            color=staticmaps.BLACK,
         )
     )
     return context.render_pillow(*sizes)
+
+def crop_image_with_borders(image):
+    width, height = image.size
+    top = 0
+    left = 0
+    right = width
+    bottom = height
+    
+    for k in range(0,width):
+        pixel = image.getpixel((k, height//2))
+        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+            left = k
+            break
+    for k in reversed(range(0,width)):
+        pixel = image.getpixel((k, height//2))
+        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+            right = k
+            break
+    for k in range(0,height):
+        pixel = image.getpixel((width//2, k))
+        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+            top = k
+            break
+    for k in reversed(range(0,height)):
+        pixel = image.getpixel((width//2, k))
+        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+            bottom = k
+            break
+
+    return image.crop((left, top, right, bottom))
 
 
 def plot_distributions(dataframe: pl.DataFrame, num_cols=4):
@@ -51,3 +83,39 @@ def plot_distributions(dataframe: pl.DataFrame, num_cols=4):
         ax = axs[i // num_cols, i % num_cols]
         ax.hist(dataframe[column])
         ax.title.set_text(column)
+
+def regula_falsi(f, a, b, tol):
+    nMax = math.ceil(math.log(abs(b-a)/tol)/math.log(2))
+    fa = f(a)
+    fb = f(b)
+    x = a
+    fx = fa
+    n = 0
+
+    while abs(a-b) >= tol+1e-16*max(abs(a),abs(b)) and abs(fx) >= tol and n < nMax:
+        n += 1
+        x = a-fa*(b-a) / (fb-fa)
+        fx = f(x)
+        if (fx >= 0 and fa >= 0) or (fa <0 and fa < 0):
+            a = x
+            fa = fx
+        else:
+            b = x
+            fb = fx
+    
+    return x, fx
+
+def find_latitude_correction(p, additional_space, b, tol=1e-4):
+    f = lambda x: distance(p, (p[0], x)) - additional_space
+    a = p[1]
+    b = a + b
+    return regula_falsi(f, a, b, tol)
+
+
+
+def distance(p1, p2):
+    lon1, lat1 = math.radians(p1[0]), math.radians(p1[1])
+    lon2, lat2 = math.radians(p2[0]), math.radians(p2[1])
+    
+    a = math.sin((lat1-lat2)/2)**2 + math.cos(lat1)*math.cos(lat2)*(math.sin((lon1-lon2)/2)**2)
+    return math.atan2(math.sqrt(a), math.sqrt(1-a))*2*6371
