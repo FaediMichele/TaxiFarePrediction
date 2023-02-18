b7a34b789eca8a72a3e9151602725114424e75c0
FaediMichele <work.faedi.michele@gmail.com>
2023-02-18 15:45:47 +0100

    Added cached image for the map(should work for different points_area) and plot hourly mean for blocks(should be interesting if splitting on neighborhood instead)

--- taxifare/basemap.py
+++ taxifare/basemap.py
@@ -1,12 +1,15 @@
 from itertools import product
 from io import BytesIO
 from PIL import Image
+from PIL.PngImagePlugin import PngInfo
 import requests
 import math
 
 URL = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
 RADIUS = 6371   # earth's average radius in km
 TILE_SIZE = 256 # each tile's size in pixels
+
+CACHED_IMAGE = 'map.png'
 
 
 def point_to_pixels(lon, lat, zoom):
@@ -32,16 +35,21 @@ def tile(session=None, **url_format_args):
         return Image.open(BytesIO(resp.content))
 
 
-def image(top, right, bottom, left, zoom=14, **tile_args):
-    """return an osm map from the given bounding box points"""
 
-    # convert gps coordinates to web mercator
-    x0, y0 = point_to_pixels(left, top, zoom)
-    x1, y1 = point_to_pixels(right, bottom, zoom)
+def load_or_download_image(x0_tile, y0_tile, x1_tile, y1_tile, zoom, tile_args):
+    data = {'x0_tile': str(x0_tile), 'y0_tile': str(y0_tile), 'x1_tile': str(x1_tile), 'y1_tile': str(y1_tile), 'zoom': str(zoom)}
+    try:
+        img = Image.open(CACHED_IMAGE)
+        if img.text == data:
+            print("Using cached image")
+            return img
+    except Exception:
+        print("Downloading image")
+        pass
 
-    # conver pixel coordinates to tiles
-    x0_tile, y0_tile = int(x0 / TILE_SIZE), int(y0 / TILE_SIZE)
-    x1_tile, y1_tile = math.ceil(x1 / TILE_SIZE), math.ceil(y1 / TILE_SIZE)
+    metadata = PngInfo()
+    for name, value in data.items():
+        metadata.add_text(name, value)
 
     # crate a single large image to add the tiles to
     img = Image.new('RGB', (
@@ -57,6 +65,23 @@ def image(top, right, bottom, left, zoom=14, **tile_args):
                 im=tile(session, x=x_tile, y=y_tile, z=zoom, **tile_args),
                 box=(int(x_tile - x0_tile) * TILE_SIZE,
                      int(y_tile - y0_tile) * TILE_SIZE))
+    img.save(CACHED_IMAGE, pnginfo=metadata)
+    return img
+
+def image(top, right, bottom, left, zoom=14, **tile_args):
+    """return an osm map from the given bounding box points"""
+
+    # convert gps coordinates to web mercator
+    x0, y0 = point_to_pixels(left, top, zoom)
+    x1, y1 = point_to_pixels(right, bottom, zoom)
+
+    # conver pixel coordinates to tiles
+    x0_tile, y0_tile = int(x0 / TILE_SIZE), int(y0 / TILE_SIZE)
+    x1_tile, y1_tile = math.ceil(x1 / TILE_SIZE), math.ceil(y1 / TILE_SIZE)
+
+    
+    img = load_or_download_image(x0_tile, y0_tile, x1_tile, y1_tile, zoom, tile_args)
+
 
     # crop image to the given bounding box
     x, y = x0_tile * TILE_SIZE, y0_tile * TILE_SIZE
